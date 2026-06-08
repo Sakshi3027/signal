88b035ae3a220ecfff4d2c8f84683f0e1240350b
Sakshi3027 <sakshchavan30@gmail.com>
2026-06-08 03:43:07 -0400

    fix: install local packages on Render

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+set -e
 pip install --upgrade pip
 pip install -r requirements.txt
+pip install -e packages/ingestion
+pip install -e packages/agent
+pip install -e packages/storage
